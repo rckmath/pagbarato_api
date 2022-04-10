6f8073b00136ce7d7179de25b74a5ca00503ffa3
Erick M. L. Pacheco <ericklopes02@gmail.com>
2022-04-10 13:24:39 -0300

    Feature: Database first migration and ERD setted up

---
+++ prisma/migrations/20220410162358_create_initial_tables/migration.sql
@@ -0,0 +1,73 @@
+-- CreateEnum
+CREATE TYPE "UserRoleType" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "PriceType" AS ENUM ('COMMON', 'DEAL');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" CHAR(36) NOT NULL,
+    "email" VARCHAR(320) NOT NULL,
+    "role" "UserRoleType" NOT NULL DEFAULT E'USER',
+    "password" TEXT,
+    "name" VARCHAR(100),
+    "birthDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Establishment" (
+    "id" CHAR(36) NOT NULL,
+    "name" TEXT NOT NULL,
+    "latitude" DECIMAL(10,6) NOT NULL,
+    "longitude" DECIMAL(10,6) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Establishment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" CHAR(36) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Price" (
+    "id" CHAR(36) NOT NULL,
+    "userId" CHAR(36),
+    "productId" CHAR(36),
+    "establishmentId" CHAR(36),
+    "value" DECIMAL(10,2) NOT NULL,
+    "type" "PriceType" NOT NULL DEFAULT E'COMMON',
+    "isProductWithNearExpirationDate" BOOLEAN NOT NULL DEFAULT false,
+    "expiresAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Price_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Price" ADD CONSTRAINT "Price_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Price" ADD CONSTRAINT "Price_establishmentId_fkey" FOREIGN KEY ("establishmentId") REFERENCES "Establishment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Price" ADD CONSTRAINT "Price_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE SET NULL ON UPDATE CASCADE;

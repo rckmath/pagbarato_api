ece0a182e0b4086a9193e860ac9930effa506bdb
Erick M. L. Pacheco <ericklopes02@gmail.com>
2022-06-06 19:48:58 -0300

    Feature: Fixed migration

---
+++ prisma/migrations/20220606223818_removed_range_from_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `preferredSearchRangeInKm` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "preferredSearchRangeInKm";
